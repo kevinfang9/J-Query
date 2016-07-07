5e336931323e4289505f844b984dee8887ca03de
Kevin <kevinfang9@gmail.com>
2016-07-07 11:23:57 +0800

    no message

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 
     
-    $("td").click(function () {
+    $("td[rowspan="2"]").click(function () {
        $(this).css("background","gold")
+    
+    });
+ $("td[rowspan="1"]").click(function () {
+       $(this).css("background","blue")
     
     });
 
