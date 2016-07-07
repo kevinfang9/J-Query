ebd597ecfbb35f0827c489e350364114c30b6eb8
Kevin <kevinfang9@gmail.com>
2016-07-07 12:10:44 +0800

    no message

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ $(document).ready(function (){
    $("td[rowspan!='2']").click(function(){
     $(this).parent().children("[rowspan='2']").css("background", "lightgreen");
 });
+$("td[colspan!='2']").click(
+function(){
+	 $("td[colspan='2']").toggle();
+});
 
    
 
